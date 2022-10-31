babbfa1444b09b35b58b76511f3d6286efcf1206
hellnomary <maria.porcires@outlook.com>
2022-10-31 17:12:57 +0100

    randomwords

--- hangman-boilerplate/js/index.js
+++ hangman-boilerplate/js/index.js
@@ -11,3 +11,14 @@
 addEventListener('keydown', (event) => {
     console.log(event.key);
 });
+
+
+const words = [`willow`, `buffy`, `xander`];
+
+const randomWord = words[Math.floor(Math.random() * words.length)]
+console.log(randomWord)
+
+
+//else if (xxxx.length !==1) {
+    //alert (`single letters only!);
+
