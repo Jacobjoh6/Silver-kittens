02631e5b0dcd186eb8a824163021f7581f494e11
Johnjr1 <114564748+Johnjr1@users.noreply.github.com>
2022-11-01 09:01:05 +0100

    hej

--- hangman-boilerplate/js/index.js
+++ hangman-boilerplate/js/index.js
@@ -7,9 +7,22 @@
  document.querySelector('figure').classList.add('legs')
 
  */
+let wordsOpt = [`hangman`, `silver`, `kittens`];
+let correctWord = document.getElementById(`correctword`);
+let wordToGuess = wordsOpt[Math.floor(Math.random() * wordsOpt.length)]
+let wordArray = wordToGuess.split('');
 
+
+let answer = null;
+console.log(wordArray);
+
+
+/*const hej = [{ bokstav1: `H`, bokstav2: `E`, bokstav3: `J` }]
 addEventListener('keydown', (event) => {
     if (event.code === 'Space') {
         console.log(`hej`);
+    } else {
+        document.querySelector('figure').classList.add('scaffold')
     }
 });
+*/
