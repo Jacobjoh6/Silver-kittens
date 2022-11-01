fffd4de4505042e814f67d68ae25f4c044d7b322
Johnjr1 <114564748+Johnjr1@users.noreply.github.com>
2022-11-01 14:25:50 +0100

    hej

--- hangman-boilerplate/js/index.js
+++ hangman-boilerplate/js/index.js
@@ -17,51 +17,77 @@ let wordArray1 = wordArray.join(" ");
 let ggText = document.querySelector(`#ggtext`);
 const gameOver = document.querySelector(`.gg`);
 const main = document.querySelector(`main`);
-let wrongGuesses = 0;
 let wrongLetter = document.querySelector(`#wrongLetter`)
-    //let letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+const bodyElem = document.querySelector('body');
+let wrongGuesses = 0;
+//let letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 gameOver.style.display = `none`;
 
-addEventListener('keydown', (event) => {
-    for (let i = 0; i < wordsOpt.length; i++) {
-        wordsOpt[i] == event.key
-        if (event.key === `m`) {
-            correctWord.innerText = wordArray1;
-        } else if (wrongGuesses === 0) {
-            document.querySelector('figure').classList.add('scaffold');
-            wrongGuesses++
-            console.log(wrongGuesses)
-        } else if (wrongGuesses === 1) {
-            document.querySelector('figure').classList.add('head')
-            wrongGuesses++
-            console.log(wrongGuesses)
-        } else if (wrongGuesses === 2) {
-            document.querySelector('figure').classList.add('body')
-            wrongGuesses++
-            console.log(wrongGuesses)
-        } else if (wrongGuesses === 3) {
-            document.querySelector('figure').classList.add('arms')
-            wrongGuesses++
-            console.log(wrongGuesses)
-        } else if (wrongGuesses === 4) {
-            document.querySelector('figure').classList.add('legs')
-            gameOver.style.display = `block`;
-            ggText.innerText = `Game Over`;
-            console.log(wrongGuesses);
-        } else {
-
-        }
-    }
-    wrongLetter.textContent += ` ` + event.key
-    console.log(event.key)
-});
-
-
-
-
-console.log(wrongGuesses)
-
-
 correctWord.innerHTML = hiddenWord1;
+
+// addEventListener('keydown', (event) => {
+//     for (let i = 0; i < wordArray.length; i++) {
+//         if (event.key === wordArray[i]) {
+//             console.log(`rätt`);
+//             break;
+//         }
+//     }
+//     wrongLetter.textContent += ` ` + event.key
+//     console.log(event.key)
+// });
+// for (let i = 0; i < wordArray.length; i++) {
+
+// }
+//  if (wrongGuesses === 0) {
+//     document.querySelector('figure').classList.add('scaffold');
+//     wrongGuesses++
+//     console.log(wrongGuesses)
+// } else if (wrongGuesses === 1) {
+//     document.querySelector('figure').classList.add('head')
+//     wrongGuesses++
+//     console.log(wrongGuesses)
+// } else if (wrongGuesses === 2) {
+//     document.querySelector('figure').classList.add('body')
+//     wrongGuesses++
+//     console.log(wrongGuesses)
+// } else if (wrongGuesses === 3) {
+//     document.querySelector('figure').classList.add('arms')
+//     wrongGuesses++
+//     console.log(wrongGuesses)
+// } else if (wrongGuesses === 4) {
+//     document.querySelector('figure').classList.add('legs')
+//     gameOver.style.display = `block`;
+//     ggText.innerText = `Game Over`;
+//     console.log(wrongGuesses);
+//}
+
+
 console.log(wordArray);
 console.log(hiddenWord);
+
+
+bodyElem.addEventListener('keydown', (event) => {
+    const keyPressed = event.key;
+    let correctGuess = false;
+
+    for (let i = 0; i < wordArray.length; i++) {
+        if (keyPressed === wordArray[i]) {
+            console.log('Rätt bokstav');
+            correctGuess = true;
+
+            // Vad ska vi göra här?
+        }
+    }
+
+    if (correctGuess === false) {
+        console.log('Fel gissning');
+        wrongLetter.textContent += ` ` + event.key
+        document.querySelector('figure').classList.add('scaffold');
+        wrongGuesses++;
+    }
+    if (correctGuess === false && wrongGuesses === 1) {
+        console.log('Fel gissning');
+        wrongLetter.textContent += ` ` + event.key
+        document.querySelector('figure').classList.add('head')
+    }
+});
