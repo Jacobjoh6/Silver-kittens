860b444e8a7753ca5a19139ceefae539d8d6dde2
Johnjr1 <114564748+Johnjr1@users.noreply.github.com>
2022-11-01 11:22:16 +0100

    Snart färdig

--- hangman-boilerplate/js/index.js
+++ hangman-boilerplate/js/index.js
@@ -18,38 +18,45 @@ const gameOver = document.querySelector(`.gg`);
 const main = document.querySelector(`main`);
 let wrongGuesses = 0;
 let wrongLetter = document.querySelector(`#wrongLetter`)
-
-wrongLetter.textContent = `Guessed letters: ` + `A`;
+    //let letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 gameOver.style.display = `none`;
 
 addEventListener('keydown', (event) => {
-    if (event.code === `Space`) {
-        correctWord.innerText = wordToGuess;
-    } else if (wrongGuesses === 0) {
-        document.querySelector('figure').classList.add('scaffold');
-        wrongGuesses++
-        console.log(wrongGuesses)
-    } else if (wrongGuesses === 1) {
-        document.querySelector('figure').classList.add('head')
-        wrongGuesses++
-        console.log(wrongGuesses)
-    } else if (wrongGuesses === 2) {
-        document.querySelector('figure').classList.add('body')
-        wrongGuesses++
-        console.log(wrongGuesses)
-    } else if (wrongGuesses === 3) {
-        document.querySelector('figure').classList.add('arms')
-        wrongGuesses++
-        console.log(wrongGuesses)
-    } else if (wrongGuesses === 4) {
-        document.querySelector('figure').classList.add('legs')
-        gameOver.style.display = `block`;
-        ggText.innerText = `Game Over`;
-        console.log(wrongGuesses);
-    } else {
+    for (let i = 0; i < wordsOpt.length; i++) {
+        wordsOpt[i] == event.key
+        if (event.code === wordsOpt[i]) {
+            correctWord.innerText = wordToGuess;
+        } else if (wrongGuesses === 0) {
+            document.querySelector('figure').classList.add('scaffold');
+            wrongGuesses++
+            console.log(wrongGuesses)
+        } else if (wrongGuesses === 1) {
+            document.querySelector('figure').classList.add('head')
+            wrongGuesses++
+            console.log(wrongGuesses)
+        } else if (wrongGuesses === 2) {
+            document.querySelector('figure').classList.add('body')
+            wrongGuesses++
+            console.log(wrongGuesses)
+        } else if (wrongGuesses === 3) {
+            document.querySelector('figure').classList.add('arms')
+            wrongGuesses++
+            console.log(wrongGuesses)
+        } else if (wrongGuesses === 4) {
+            document.querySelector('figure').classList.add('legs')
+            gameOver.style.display = `block`;
+            ggText.innerText = `Game Over`;
+            console.log(wrongGuesses);
+        } else {
 
+        }
     }
+    wrongLetter.textContent += ` ` + event.key
+    console.log(event.key)
 });
+
+
+
 
 console.log(wrongGuesses)
 
